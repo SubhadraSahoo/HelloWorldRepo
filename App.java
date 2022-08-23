8fc9bac91d1788857405ab40123fd109e4e07c59
SubhadraSahoo <75673851+SubhadraSahoo@users.noreply.github.com>
2022-08-23 20:33:23 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package subhadra_test.subhadra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
